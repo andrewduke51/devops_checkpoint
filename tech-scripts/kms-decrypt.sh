0f5bae4d8d89ab65636ab8b584f875e546d7ce31
andrewduke51 <andrew.figaroa@snagajob.com>
2018-07-19 08:05:25 -0400

    add tech-scripts

--- tech-scripts/kms-decrypt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DECRYPT_THIS_BASE64_STRING=fileb://"${3:-secret}"
-PROFILE="${2:-dev}"
-
-output=$(aws kms decrypt --ciphertext-blob fileb://<(echo $DECRYPT_THIS_BASE64_STRING | base64 -D) --query Plaintext --output text --profile ${PROFILE} | base64 -D)
-echo "Decrypted Ciphertext Is: $output"
-echo -n "$output" > /tmp/output.txt
