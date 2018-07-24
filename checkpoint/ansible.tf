b53e465c1584854ec07c4d00869cb5c50c30a350
andrewduke51 <andrew.figaroa@snagajob.com>
2018-07-24 09:35:34 -0400

    race condition fixed

---
+++ checkpoint/ansible.tf
@@ -0,0 +1,32 @@
+## web_server ##
+
+//data "template_file" "web_server" {
+//  template = "${file("${path.module}/templates/ansible.sh.tpl")}"
+//}
+//
+//data "template_cloudinit_config" "ansible_init" {
+//  gzip          = false
+//  base64_encode = false
+//
+//  part {
+//    filename     = "ansible.sh"
+//    content_type = "text/x-shellscript"
+//    content      = "${data.template_file.web_server.rendered}"
+//  }
+//}
+
+resource "aws_instance" "web_server" {
+  ami                    = "ami-b70554c8"
+  instance_type          = "t2.micro"
+  subnet_id              = "${aws_subnet.checkpoint_dmz.id}"
+  vpc_security_group_ids = ["${aws_security_group.checkpoint_dmz.id}"]
+  key_name               = "${var.DEPLOY_KEY}"
+  availability_zone      = "${var.AVAILABILTY_ZONE}"
+
+  //  user_data                   = "${data.template_cloudinit_config.ansible_init.rendered}"
+  associate_public_ip_address = true
+
+  tags {
+    Name = "checkpoint_ansible_new"
+  }
+}
