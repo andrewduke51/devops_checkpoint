e8c39d1b2ae5868dbad8294f50da8471b8315aff
andrewduke51 <andrew.figaroa@snagajob.com>
2018-07-25 14:32:09 -0400

    race condition fixed

--- checkpoint/variables.tf
+++ checkpoint/variables.tf
@@ -1,41 +1,21 @@
 variable "AWS_PROFILE" {}
-
 variable "PATH_TO_CRED" {}
-
 variable "NAT_PUBLIC_IP" {}
-
 variable "DEFAULT_REGION" {}
-
 variable "ORIGIN_IPS" {}
-
 variable "GW_PRIVATE_IP_ETH0" {}
-
 variable "GW_PRIVATE_IP_ETH1" {}
-
 variable "GW_PUBLIC_IP" {}
-
 variable "AVAILABILTY_ZONE" {}
-
-variable "DEPLOY_KEY" {}
-
 variable "MANAGER_PRIVATE_IP" {}
-
 variable "MANAGER_PUBLIC_IP" {}
-
 variable "PATH_TO_PEM" {}
-
 variable "PATH_TO_ANSIBLE_YAML" {}
-
 variable "ANSIBLE_PRIVATE_IP" {}
-
 variable "SSH_PUB_KEY" {}
-
 variable "SSH_PRIVATE_KEY" {}
-
 variable "PATH_TO_RM_SSH" {}
-
 variable "CHECKPOINT_INSTANCE_TYPE" {}
-
 variable "DOWNTIME_TAG" {}
-
 variable "ANSIBLE_PUBLIC_IP" {}
+variable "GAIA_PASS " {}
